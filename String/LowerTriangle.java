4cd9f776f320351ad2a311b19e449b9a5a7b7426
AshishSharma0704 <160305996+AshishSharma0704@users.noreply.github.com>
2026-04-13 22:17:59 +0530

    Lower Triangle, coding blocks

---
+++ String/LowerTriangle.java
@@ -0,0 +1,28 @@
+package String;
+import java.util.*;
+public class LowerTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[][] arr=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(arr[i][j]==0){
+                    ans=1;
+                    break;
+                }
+            }
+        }
+        if(ans==0){
+            System.out.println("false");
+        }else{
+            System.out.println("true");
+        }
+    }
+}
